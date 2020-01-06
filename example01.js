0b7f2e1fb88c3ebde1a7b9eb8808d5fa7d3e6cf5
Gwan Woo <skymill2000@gmail.com>
2020-01-06 16:28:45 +0900

    init Commit

---
+++ example01.js
@@ -0,0 +1,22 @@
+
+var length = 16; // Number
+var lastName = "Johnson"; // String
+var x = { firstName: "John", lastName: "Doe" }; // Object
+
+console.log(x);
+
+
+var w = 16 + "Volvo";
+console.log(w);
+
+var y = "16" + "Volvo";
+console.log(y);
+
+var z = 16 + "Volvo";
+console.log(z);
+
+var a = "Volvo" + 16;
+console.log(a);
+
+var b = 16 + 4 + "Volvo";
+console.log(b);
