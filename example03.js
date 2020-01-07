62316605ba74efda525161264212b6690037a160
Gwan Woo <skymill2000@gmail.com>
2020-01-07 15:21:30 +0900

    array end

---
+++ example03.js
@@ -0,0 +1,41 @@
+var cars = [];
+
+var car01 = {
+	name : "sonata",
+	ph : "500ph",
+	start : function () {
+		console.log("engine is starting");
+	},
+	stop : function () {
+		console.log("engine is stoped");
+	}
+}
+
+var car02 = {
+	name : "bmw",
+	ph : "500ph",
+	start : function () {
+		console.log("engine is starting");
+	},
+	stop : function () {
+		console.log("engine is stoped");
+	}
+}
+
+
+var car03 = {
+	name : "poter",
+	ph : "500ph",
+	start : function () {
+		console.log("engine is starting");
+	},
+	stop : function () {
+		console.log("engine is stoped");
+	}
+}
+
+cars[0] = car01
+cars[1] = car02
+cars[2] = car03
+
+console.log(cars);
