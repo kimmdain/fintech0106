f472b8b259e173bbddab8f79c40becafe48cc2ca
Gwan Woo <skymill2000@gmail.com>
2020-01-07 16:40:10 +0900

    request 실습

--- async.js
+++ async.js
@@ -1,15 +1,20 @@
 var fs = require('fs');
 
 console.log('첫번째 기능입니다.');
-fs.readFile('./example/text.txt', 'utf8', function (err, result) {
-    if (err) {
-        console.error(err);
-        throw err;
-    }
-    else {
-        console.error("두번째 기능인데 파일을 읽어오느라 시간이... 조큼 ... 걸려요");
-        console.log(result);
-    }
-});
-console.log('마지막 기능입니다.');
+function callbackFunc(callback){
+    fs.readFile('./example/text.txt', 'utf8', function (err, result) {
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+        else {
+            console.error("두번째 기능인데 파일을 읽어오느라 시간이... 조큼 ... 걸려요");
+            callback(result)
+        }
+    });
+}
 
+callbackFunc(function(readResult){
+    console.log(readResult)
+    console.log("c")
+})
