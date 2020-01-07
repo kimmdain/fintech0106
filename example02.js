4fac3bcae8f74b6a66c8159dc5c0f9defc558feb
Gwan Woo <skymill2000@gmail.com>
2020-01-07 15:05:06 +0900

    example02.js is added

---
+++ example02.js
@@ -0,0 +1,25 @@
+function mul(p1, p2){
+    return p1 * p2;
+}
+
+function plus(p1, p2){
+    return p1 + p2;
+}
+
+function minus(p1, p2){
+    return p1 - p2;
+}
+
+function div(p1, p2){
+    return p1 / p2;
+}
+
+
+//----------------------
+//Todo +-/ 기능 추가
+//-----------------------
+var resultMul = mul(5,5);
+console.log(resultMul);
+console.log(plus(5,5));
+console.log(minus(5,5));
+console.log(div(5,5));
