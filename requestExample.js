67059bbd0a583836f8e9bc0cb2de4123ad68ef3c
Gwan Woo <skymill2000@gmail.com>
2020-01-07 16:56:46 +0900

    request 완성

--- requestExample.js
+++ requestExample.js
@@ -4,6 +4,6 @@ var parseString = require('xml2js').parseString;
 request('http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109', function (error, response, body) {
 //   console.log('body:', body);
   parseString(body, function (err, result) {
-    console.dir(result.rss.channel[0]);
+    console.dir(result.rss.channel[0].item[0].description[0].header[0].wf);
   });
 });
