648539f76215e996e60b1ac79716d7cec535e27c
Gwan Woo <skymill2000@gmail.com>
2020-01-08 12:54:29 +0900

    EJS 샘플 코드 추가

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require("express"),
 app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.get("/sayHello", function (request, response) {
-	var user_name = request.query.user_name;
-	response.end("Hello " + user_name + "!");
-});
+app.get('/test', function(req, res){
+    res.render('test');
+})
 
 app.listen(port);
 console.log("Listening on port ", port);
